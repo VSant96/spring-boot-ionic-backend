7665a7731d86cdc839d09f647653aae04adf80fa
Victor Santos <vsdeveloper96@outlook.com>
2018-09-19 16:50:53 -0300

    Pedido, Pagamento, EstadoPagamento

--- src/main/java/com/victor/cursomc/CursomcApplication.java
+++ src/main/java/com/victor/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.victor.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.victor.cursomc.domain.Cidade;
 import com.victor.cursomc.domain.Cliente;
 import com.victor.cursomc.domain.Endereco;
 import com.victor.cursomc.domain.Estado;
+import com.victor.cursomc.domain.Pagamento;
+import com.victor.cursomc.domain.PagamentoComBoleto;
+import com.victor.cursomc.domain.PagamentoComCartao;
+import com.victor.cursomc.domain.Pedido;
 import com.victor.cursomc.domain.Produto;
+import com.victor.cursomc.domain.enums.EstadoPagamento;
 import com.victor.cursomc.domain.enums.TipoCliente;
 import com.victor.cursomc.repositories.CategoriaRepository;
 import com.victor.cursomc.repositories.CidadeRepository;
 import com.victor.cursomc.repositories.ClienteRepository;
 import com.victor.cursomc.repositories.EnderecoRepository;
 import com.victor.cursomc.repositories.EstadoRepository;
+import com.victor.cursomc.repositories.PagamentoRepository;
+import com.victor.cursomc.repositories.PedidoRepository;
 import com.victor.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
+	@Autowired
+	PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -80,12 +94,26 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null,sdf.parse("30/09/2017 10:32"),cli1,e1);
+		Pedido ped2 = new Pedido(null,sdf.parse("10/10/2017 19:35"),cli1,e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);	
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
 		
 	
