2dfe95d9d7008b30478845a3bb61a555d38c31d1
Victor Santos <vsdeveloper96@outlook.com>
2018-09-24 14:23:58 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/victor/cursomc/config/TestConfig.java
+++ src/main/java/com/victor/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.victor.cursomc.services.DBService;
+import com.victor.cursomc.services.EmailService;
+import com.victor.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -20,6 +22,12 @@ public class TestConfig {
 	{
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() 
+	{
+		return new MockEmailService();
 	}
 
 }
--- src/main/java/com/victor/cursomc/resources/PedidoResource.java
+++ src/main/java/com/victor/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.victor.cursomc.domain.Categoria;
 import com.victor.cursomc.domain.Pedido;
-import com.victor.cursomc.dto.CategoriaDTO;
 import com.victor.cursomc.services.PedidoService;
 
 @RestController
